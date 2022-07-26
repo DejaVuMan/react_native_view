0043a3d5d98ec33617ed9bc8f59374927ad7b77c
Alan Szuszkiewicz <szuszkiewiczalan928@gmail.com>
2022-07-26 13:13:53 -0500

    Removed unnecessary elements

--- App.js
+++ App.js
@@ -8,20 +8,8 @@
 
 import React from 'react';
 import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-} from 'react-native';
 
-import {
-  Colors,
-} from 'react-native/Libraries/NewAppScreen';
-
-import { Provider as PaperProvider, TextInput } from 'react-native-paper';
+import { Provider as PaperProvider } from 'react-native-paper';
 import { LoginScreen } from './app/screens/login/login.screen';
 import { theme } from './App.style';
 
